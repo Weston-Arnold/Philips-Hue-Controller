11bc05d8e47a2184f9b216aa1bb2d9b1df0f59c7
Weston Arnold <tallonxvii@gmail.com>
2021-09-14 01:02:39 -0500

    wip: basic connection made

--- PhilipsHueController/PhilipsHueController/MainWindow.cs
+++ PhilipsHueController/PhilipsHueController/MainWindow.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Q42.HueApi;
+using System;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace PhilipsHueController
@@ -10,9 +12,26 @@ namespace PhilipsHueController
             InitializeComponent();
         }
 
-        private void MainWindow_Load(object sender, EventArgs e)
+        private async void MainWindow_Load(object sender, EventArgs e)
         {
+            await SetupHueBridge();
+        }
 
+        private async Task SetupHueBridge()
+        {
+            var locator = new HttpBridgeLocator();
+            
+            await locator.LocateBridgesAsync(TimeSpan.FromSeconds(5));
+            await HueBridgeDiscovery.CompleteDiscoveryAsync(TimeSpan.FromSeconds(5), TimeSpan.FromSeconds(30));
+
+            var localHueClient = new LocalHueClient("192.168.1.64");
+            var appKey = await localHueClient.RegisterAsync("PhilipsHueController", "MyPC");
+
+            localHueClient.Initialize(appKey);
+
+            var command = new LightCommand();
+            command.TurnOn();
+            await localHueClient.SendCommandAsync(command);
         }
     }
 }
