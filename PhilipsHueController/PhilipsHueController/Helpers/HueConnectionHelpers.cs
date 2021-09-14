d12276dfb25695a30c12fa90951a7b7a7859e77a
Weston Arnold <tallonxvii@gmail.com>
2021-09-14 01:22:20 -0500

    feat: setup helper and basic connectivity

---
+++ PhilipsHueController/PhilipsHueController/Helpers/HueConnectionHelpers.cs
@@ -0,0 +1,26 @@
+﻿using Q42.HueApi;
+using System;
+using System.Threading.Tasks;
+
+namespace PhilipsHueController
+{
+    public static class HueConnectionHelpers
+    {
+        public static async Task ConfigureBridge()
+        {
+            var locator = new HttpBridgeLocator();
+
+            await locator.LocateBridgesAsync(TimeSpan.FromSeconds(5));
+            await HueBridgeDiscovery.CompleteDiscoveryAsync(TimeSpan.FromSeconds(5), TimeSpan.FromSeconds(30));
+
+            var localHueClient = new LocalHueClient("192.168.1.64");
+            var appKey = await localHueClient.RegisterAsync("PhilipsHueController", "MyPC");
+
+            localHueClient.Initialize(appKey);
+
+            var command = new LightCommand();
+            command.TurnOn();
+            await localHueClient.SendCommandAsync(command);
+        }
+    }
+}
--- PhilipsHueController/PhilipsHueController/MainWindow.cs
+++ PhilipsHueController/PhilipsHueController/MainWindow.cs
@@ -1,6 +1,4 @@
-﻿using Q42.HueApi;
-using System;
-using System.Threading.Tasks;
+﻿using System;
 using System.Windows.Forms;
 
 namespace PhilipsHueController
@@ -14,24 +12,7 @@ namespace PhilipsHueController
 
         private async void MainWindow_Load(object sender, EventArgs e)
         {
-            await SetupHueBridge();
-        }
-
-        private async Task SetupHueBridge()
-        {
-            var locator = new HttpBridgeLocator();
-            
-            await locator.LocateBridgesAsync(TimeSpan.FromSeconds(5));
-            await HueBridgeDiscovery.CompleteDiscoveryAsync(TimeSpan.FromSeconds(5), TimeSpan.FromSeconds(30));
-
-            var localHueClient = new LocalHueClient("192.168.1.64");
-            var appKey = await localHueClient.RegisterAsync("PhilipsHueController", "MyPC");
-
-            localHueClient.Initialize(appKey);
-
-            var command = new LightCommand();
-            command.TurnOn();
-            await localHueClient.SendCommandAsync(command);
+            await HueConnectionHelpers.ConfigureBridge();
         }
     }
 }
