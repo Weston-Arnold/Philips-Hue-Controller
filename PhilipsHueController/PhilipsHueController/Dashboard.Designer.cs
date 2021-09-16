de125ff08e1144f460cb6b2e985b3e31407f3fc0
Weston Arnold <tallonxvii@gmail.com>
2021-09-15 22:31:35 -0500

    feat: add additional information

--- PhilipsHueController/PhilipsHueController/Dashboard.Designer.cs
+++ PhilipsHueController/PhilipsHueController/Dashboard.Designer.cs
@@ -36,6 +36,8 @@ namespace PhilipsHueController
             this.lbLights = new System.Windows.Forms.ListBox();
             this.lblLightListHeader = new System.Windows.Forms.Label();
             this.btnRename = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.pnlContinueSetup.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -56,7 +58,7 @@ namespace PhilipsHueController
             this.pnlContinueSetup.Controls.Add(this.btnCompleteSetup);
             this.pnlContinueSetup.Location = new System.Drawing.Point(2, -3);
             this.pnlContinueSetup.Name = "pnlContinueSetup";
-            this.pnlContinueSetup.Size = new System.Drawing.Size(51, 51);
+            this.pnlContinueSetup.Size = new System.Drawing.Size(1259, 67);
             this.pnlContinueSetup.TabIndex = 1;
             // 
             // lblSetupRequiredTitle
@@ -87,7 +89,8 @@ namespace PhilipsHueController
             this.lbLights.ItemHeight = 25;
             this.lbLights.Location = new System.Drawing.Point(27, 116);
             this.lbLights.Name = "lbLights";
-            this.lbLights.Size = new System.Drawing.Size(333, 979);
+            this.lbLights.ScrollAlwaysVisible = true;
+            this.lbLights.Size = new System.Drawing.Size(333, 379);
             this.lbLights.TabIndex = 2;
             this.lbLights.SelectedIndexChanged += new System.EventHandler(this.lbLights_SelectedIndexChanged);
             // 
@@ -103,7 +106,7 @@ namespace PhilipsHueController
             // 
             // btnRename
             // 
-            this.btnRename.Location = new System.Drawing.Point(27, 1117);
+            this.btnRename.Location = new System.Drawing.Point(27, 1132);
             this.btnRename.Name = "btnRename";
             this.btnRename.Size = new System.Drawing.Size(333, 65);
             this.btnRename.TabIndex = 4;
@@ -111,11 +114,32 @@ namespace PhilipsHueController
             this.btnRename.UseVisualStyleBackColor = true;
             this.btnRename.Click += new System.EventHandler(this.btnRename_Click);
             // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Enabled = false;
+            this.richTextBox1.Location = new System.Drawing.Point(27, 629);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(333, 434);
+            this.richTextBox1.TabIndex = 5;
+            this.richTextBox1.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(27, 576);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(317, 41);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Additional Information";
+            // 
             // Dashboard
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1893, 1209);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.btnRename);
             this.Controls.Add(this.lblLightListHeader);
             this.Controls.Add(this.lbLights);
@@ -141,5 +165,7 @@ namespace PhilipsHueController
         private System.Windows.Forms.ListBox lbLights;
         private System.Windows.Forms.Label lblLightListHeader;
         private System.Windows.Forms.Button btnRename;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
     }
 }
